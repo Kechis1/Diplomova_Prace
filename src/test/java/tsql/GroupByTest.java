186e1ef4c9fd57e045a863e7d1438071575f4c01
Kechis1 <danielbill95@gmail.com>
2020-09-25 20:10:01 +0200

    created unit tests for group by

---
+++ src/test/java/tsql/GroupByTest.java
@@ -0,0 +1,75 @@
+package tsql;
+
+import DP.Database.DatabaseMetadata;
+import DP.antlr4.tsql.TSqlRunner;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.Mock;
+import name.falgout.jeffrey.testing.junit.mockito.MockitoExtension;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import java.io.IOException;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.*;
+
+@ExtendWith(MockitoExtension.class)
+public class GroupByTest {
+    @Mock
+    private DatabaseMetadata metadata;
+
+    @BeforeEach
+    void init() {
+        metadata = DatabaseMetadata.LoadFromJson("databases/db_student_studuje_predmet.json");
+        System.out.println("before each");
+    }
+
+    @ParameterizedTest(name="doFindUnnecessaryGroupByTest {index} query = {0}")
+    @MethodSource("doFindUnnecessaryGroupBySource")
+    void doFindUnnecessaryGroupByTest(String query) throws IOException {
+        boolean result = TSqlRunner.RunGroupByWithPrimaryKey(metadata, query);
+        assertFalse(result);
+    }
+
+    @ParameterizedTest(name="doFindNecessaryGroupByTest {index} query = {0}")
+    @MethodSource("doFindNecessaryGroupBySource")
+    void doFindNecessaryGroupByTest(String query) throws IOException {
+        boolean result = TSqlRunner.RunGroupByWithPrimaryKey(metadata, query);
+        assertTrue(result);
+    }
+
+    @ParameterizedTest(name="doFindRewrittenableAggregateFunctionsTest {index} query = {0}")
+    @MethodSource("doFindRewrittenableAggregateFunctionsSource")
+    void doFindRewrittenableAggregateFunctionsTest(String query) throws IOException {
+        boolean result = TSqlRunner.RunGroupByWithPrimaryKey(metadata, query);
+        assertFalse(result);
+    }
+
+
+    public static Stream<Arguments> doFindUnnecessaryGroupBySource() {
+        return Stream.of(Arguments.arguments("SELECT PID, JMENO FROM DBO.PREDMET GROUP BY PID, JMENO"),
+                Arguments.arguments("SELECT pr.pId, stt.sID, ste.sID, ste.pID FROM dbo.student stt JOIN dbo.studuje ste ON stt.sID = ste.sID JOIN dbo.predmet pr ON ste.pID = pr.pID GROUP BY pr.pID, stt.sID, ste.pID, ste.sID, ste.rok")
+        );
+    }
+
+    public static Stream<Arguments> doFindNecessaryGroupBySource() {
+        return Stream.of(Arguments.arguments("SELECT pId, jmeno FROM dbo.predmet GROUP BY pid, jmeno HAVING sum(pid) > 3"),
+                Arguments.arguments("SELECT pId, jmeno FROM dbo.predmet GROUP BY pid, jmeno ORDER BY sum(pid)"),
+                Arguments.arguments("SELECT pr.pId, stt.sID FROM dbo.student stt JOIN dbo.studuje ste ON stt.sID = ste.sID JOIN dbo.predmet pr ON ste.pID = pr.pID GROUP BY pr.pID, stt.sID"),
+                Arguments.arguments("SELECT jmeno, count(pt.pID) FROM predmet pt JOIN studuje st on pt.pID = st.pID GROUP BY jmeno, pt.pID")
+        );
+    }
+
+    public static Stream<Arguments> doFindRewrittenableAggregateFunctionsSource() {
+        return Stream.of(
+                Arguments.arguments("SELECT pId, jmeno, sum(pid) FROM dbo.predmet GROUP BY pid, jmeno"),
+                Arguments.arguments("SELECT pId, jmeno, count(*) FROM dbo.predmet GROUP BY pid, sqrt(jmeno), jmeno"),
+                Arguments.arguments("SELECT pId, jmeno, count(pId) FROM dbo.predmet GROUP BY pid, jmeno"),
+                Arguments.arguments("SELECT pId, jmeno, sum(pId) FROM dbo.predmet GROUP BY pid, jmeno")
+        );
+    }
+
+}

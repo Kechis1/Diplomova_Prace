08ff85b6cad792d2075832de299da4b6f1f436cf
Kechis1 <danielbill95@gmail.com>
2021-02-02 22:19:23 +0100

    added normalize query

--- src/main/java/DP/Transformations/QueryHandler.java
+++ src/main/java/DP/Transformations/QueryHandler.java
@@ -5,6 +5,7 @@ import DP.antlr4.tsql.parser.TSqlLexer;
 import DP.antlr4.tsql.parser.TSqlParser;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
 
 public abstract class QueryHandler implements ITransformation {
     private final QueryHandler next;
@@ -28,7 +29,18 @@ public abstract class QueryHandler implements ITransformation {
     }
 
     public void normalizeQuery(Query query) {
-
+        TSqlParser parser = parseQuery(query.getCurrentQuery());
+        ParseTree select = parser.select_statement();
+        String parsedQuery = select.getText();
+        int index = -1;
+        do {
+            index = query.getCurrentQuery().indexOf(' ', index + 1);
+            if (index != -1) {
+                parsedQuery = (parsedQuery.substring(0, index) + " " + parsedQuery.substring(index)).trim();
+            }
+        } while (index >= 0);
+        query.setCurrentQuery(parsedQuery);
+        query.getQueryTransforms().get(query.getQueryTransforms().size() - 1).setOutputQuery(parsedQuery);
     }
 
     public DatabaseMetadata getDatabaseMetadata() {

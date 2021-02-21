9d42e9fe7ac488858749eaf73c50e2d097fe7f14
danielbill <danielbill95@gmail.com>
2021-02-21 17:01:00 +0100

    added option to enter query in to console

--- src/main/java/DP/Main.java
+++ src/main/java/DP/Main.java
@@ -2,12 +2,49 @@ package DP;
 
 import DP.Database.DatabaseMetadata;
 import DP.Transformations.*;
+import DP.antlr4.tsql.parser.TSqlParser;
+
+import java.io.InputStream;
+import java.util.Scanner;
 
 public class Main {
+    private static final String pathToMetadata = "databases/db_student_studuje_predmet.json";
+
     public static void main(String[] args) {
-        // init data
-        DatabaseMetadata metadata = DatabaseMetadata.LoadFromJson("databases/db_student_studuje_predmet.json");
-        String requestQuery = "SELECT * FROM DBO.STUDENT SDT INNER JOIN DBO.STUDUJE SDE ON SDT.SID = SDE.SID INNER JOIN DBO.PREDMET PDT ON SDT.SID = SDE.SID AND SDE.PID = PDT.PID";
+        Scanner in = new Scanner(System.in);
+        String inQuery;
+        String inPathToMetadata;
+        boolean repeat;
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        InputStream is;
+
+        do {
+            System.out.println("Enter query");
+            inQuery = in.nextLine().toUpperCase().trim();
+            if (inQuery.isEmpty()) {
+                repeat = true;
+            } else {
+                TSqlParser parser = QueryHandler.parseQuery(inQuery);
+                parser.select_statement();
+                repeat = parser.getErrorHandler().inErrorRecoveryMode(parser);
+            }
+        } while (repeat);
+
+        do {
+            System.out.println("Enter path to database metadata (optional)");
+            inPathToMetadata = in.nextLine().trim();
+            is = classloader.getResourceAsStream(inPathToMetadata);
+        } while (is == null);
+
+        if (inPathToMetadata.isEmpty()) {
+            runExample(inQuery, pathToMetadata);
+        } else {
+            runExample(inQuery, inPathToMetadata);
+        }
+    }
+
+    private static void runExample(String requestQuery, String requestMetadata) {
+        DatabaseMetadata metadata = DatabaseMetadata.LoadFromJson(requestMetadata);
         Query query = new Query(requestQuery, requestQuery);
 
         runChain(metadata, query);
@@ -20,7 +57,7 @@ public class Main {
         printRuns(query);
 
         System.out.println("---");
-        System.out.println(query);
+        System.out.println("Result query: " + query.getCurrentQuery());
     }
 
     private static void runChain(Query query, QueryHandler chain) {
@@ -29,8 +66,8 @@ public class Main {
 
         printRuns(query);
 
-       System.out.println("---");
-        System.out.println(query);
+        System.out.println("---");
+        System.out.println("Result query: " + query.getCurrentQuery());
     }
 
     private static void printRuns(Query query) {
